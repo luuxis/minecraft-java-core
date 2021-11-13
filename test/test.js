f58e375a2db79469b92f5c8e0669faa8480e71ea
andre <61792917+luuxis@users.noreply.github.com>
2021-11-13 22:27:04 +0100

    fix

--- test/test.js
+++ test/test.js
@@ -1,7 +1,4 @@
 const { handler } = require('../index.js');
-const json = require('./minecraft/versions/1.12.2/1.12.2.json');
-const fs = require('fs');
 
 
-//handler.getData("http://uzurion.luuxis.fr/files/test", "./minecraft")
-fs.writeFileSync(`config.json`, JSON.stringify(json, true, 4), 'UTF-8')
+handler.getData("http://uzurion.luuxis.fr/files/test", "./minecraft")
