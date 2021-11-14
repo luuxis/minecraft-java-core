b6b6a9dba122acd62932e6781792273624592782
andre <61792917+luuxis@users.noreply.github.com>
2021-11-14 04:50:44 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,4 +1,8 @@
 const { handler } = require('../index.js');
+const extractNatives = require('../../Minecraft-Java-Core/utils/native.js');
 
+handler.getData("http://uzurion.luuxis.fr/files/test", "./minecraft").then(() => {
+    console.log("done");
+    extractNatives.extractNatives("1.12.2", "./minecraft")
+});
 
-handler.getData("http://uzurion.luuxis.fr/files/test", "./minecraft")
