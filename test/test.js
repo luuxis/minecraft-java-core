ff46a4ee06f9434c5ccda47dd330731769998092
andre <61792917+luuxis@users.noreply.github.com>
2021-11-14 05:16:35 +0100

    update

--- test/test.js
+++ test/test.js
@@ -4,5 +4,7 @@ const extractNatives = require('../../Minecraft-Java-Core/utils/native.js');
 handler.getData("http://uzurion.luuxis.fr/files/test", "./minecraft").then(() => {
     console.log("done");
     extractNatives.extractNatives("1.12.2", "./minecraft")
+}).catch(err => {
+    console.log(err);
 });
 
--- utils/native.js
+++ utils/native.js
@@ -3,14 +3,14 @@ const os = require('os');
 const fs = require('fs');
 const zip = require('adm-zip');
 let minecraftVersionsJson = "https://launchermeta.mojang.com/mc/game/version_manifest_v2.json";
-let platform = { linux: "linux", win32: "windows" }
+let platform = { linux: "linux", win32: "windows", darwin: "osx" }
 
 
 module.exports.extractNatives = async function (minecraftVersion, path) {
   if (!fs.existsSync(`${path}/versions/${minecraftVersion}/natives/`)){
-    fs.mkdir(`${path}/versions/${minecraftVersion}/natives/`, { recursive: true })
+    fs.mkdirSync(`${path}/versions/${minecraftVersion}/natives/`, { recursive: true })
   }
-
+  
   let minecraftVersionJson = await fetch(minecraftVersionsJson).then(res => res.json());
   let minecraftVersionUrl;
   minecraftVersionJson.versions.forEach(element => {
@@ -18,23 +18,20 @@ module.exports.extractNatives = async function (minecraftVersion, path) {
       minecraftVersionUrl = element.url;
     }
   });
-
+  
   let minecraftNatives = await fetch(minecraftVersionUrl).then(res => res.json());
   minecraftNatives.libraries.forEach(element => {
     if (element.downloads.classifiers) {
       let native = element.downloads.classifiers[`natives-${platform[os.platform()]}`];
-      const name = native.path.split('/').pop();
-      if (os.platform() == "win32" || os.platform() == "linux") {
+      if (os.platform() == "win32" || os.platform() == "linux" || os.platform() == "darwin") {
         if (element.downloads.classifiers[`natives-${platform[os.platform()]}`]) {
-            new zip(`${path}/libraries/${native.path}`).extractAllTo(`${path}/versions/${minecraftVersion}/natives/`, true);
-        }
-      } else if (os.platform() == "darwin") {
-        if (element.downloads.classifiers[`natives-oxs`] || element.downloads.classifiers[`natives-macos`]) {
+          try {
             new zip(`${path}/libraries/${native.path}`).extractAllTo(`${path}/versions/${minecraftVersion}/natives/`, true)
+          } catch (err) {}
         }
       } else {
         console.warn(`[!] Unsupported OS: ${os.platform()}`);
       }
     }
-  });
+  })
 }
