10b3c1d739855f315b6349ebdc5d4fb100cf33e7
andre <61792917+luuxis@users.noreply.github.com>
2021-11-21 13:19:01 +0100

    Update test.js

--- test.js
+++ test.js
@@ -5,13 +5,14 @@ const fs = require('fs');
 
 
 async function main() {
-let opts = {
-    path: "./minecraft",
-    url: "http://uzurion.luuxis.fr/test/",
-    custom: true
-}
-minecraft.launch(opts)
-fs.writeFileSync(`config.json`, JSON.stringify(await await minecraft.checkFiles(), true, 4), 'UTF-8')
-console.log(await minecraft.checkFiles());
+    let opts = {
+        path: "./minecraft",
+        url: "http://uzurion.luuxis.fr/test/",
+        custom: true
+    }
+    
+    minecraft.launch(opts)
+    fs.writeFileSync(`config.json`, JSON.stringify(await await minecraft.checkFiles(), true, 4), 'UTF-8')
+    console.log(await minecraft.checkFiles());
 }
 main()
