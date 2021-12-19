42a352600bcd5387530d269fc3857106ad6d0219
andre <61792917+luuxis@users.noreply.github.com>
2021-12-19 18:53:47 +0100

    Update index.js

--- test/index.js
+++ test/index.js
@@ -11,7 +11,6 @@ let opts = {
     detached: false,
 
     java: true,
-    javapath: "C:/Users/Luuxis/Desktop/Minecraft/Minecraft/runtime/jre-legacy/windows-x64/jre-legacy/bin/java.exe",
     custom: true,
 
     verify: true,
