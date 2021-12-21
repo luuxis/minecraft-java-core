7190501045aac2effe60ca6d6de240014ab10ed0
andre <61792917+luuxis@users.noreply.github.com>
2021-12-21 17:21:25 +0100

    add Verif type browser

--- utils/authenticator/microsoft.js
+++ utils/authenticator/microsoft.js
@@ -1,4 +1,24 @@
 const fetch = require('node-fetch');
 
-class Microsoft {
+const MICROSOFT_LIVE_LOGIN_URL = 'https://login.live.com';
+const MICROSOFT_XBOX_LOGIN_URL = 'https://user.auth.xboxlive.com';
+const MICROSOFT_XSTS_AUTH_URL = 'https://xsts.auth.xboxlive.com';
+const MICROSOFT_OAUTH_REDIRECT_URL ='https://login.microsoftonline.com/common/oauth2/nativeclient';
+const MINECRAFT_SERVICES_URL = 'https://api.minecraftservices.com';
+let type;
+
+
+if(process.versions.electron) {
+    type = 'electron';
+} else if(process.versions.nw) {
+    type = 'nwjs';
+} else {
+    console.log("Running in browser")
 }
+
+// class Microsoft {
+// }
+
+// new Microsoft();
+
+console.log(type);
