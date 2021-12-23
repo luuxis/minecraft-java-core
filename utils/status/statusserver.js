3aaa50424b2a53d4c173fa7600a780f7313e58b3
andre <61792917+luuxis@users.noreply.github.com>
2021-12-23 19:23:01 +0100

    fix satus server port

--- utils/status/statusserver.js
+++ utils/status/statusserver.js
@@ -1,6 +1,6 @@
 const Gamedig = require('gamedig');
 
 module.exports.StatusServer = async function(ip, port = 25565) {
-    let result = Gamedig.query({type: 'minecraft',host: ip,port: port}).then(res => res).catch(err => false);
+    let result = Gamedig.query({type: 'minecraft',host: ip,port: port}).then(res => res).catch(err => {console.log(err); false});
     return result;
 }
