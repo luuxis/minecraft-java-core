89bf772e7f885cb4b34e8044d95a90e0eadab11b
andre <61792917+luuxis@users.noreply.github.com>
2021-12-23 11:17:26 +0100

    Update electron.js

--- utils/authenticator/GUI/electron.js
+++ utils/authenticator/GUI/electron.js
@@ -1,18 +1,11 @@
 const path = require('path')
+const {app, BrowserWindow} = require('electron')
 
+const defaultProperties = {
+  width: 1000,
+  height: 650,
+  resizable: false,
+  center: true,
+  icon: path.join(__dirname, '../../../assets/icons', `microsoft.${(process.platform === 'win32') ? 'ico' : 'png'}`),
+};
 
-module.exports.getPlatformIcon = function (filename){
-    let ext
-    switch(process.platform) {
-        case 'win32':
-            ext = 'ico'
-            break
-        case 'darwin':
-        case 'linux':
-        default:
-            ext = 'png'
-            break
-    }
-
-    return path.join(__dirname, '../../../assets/icons', `${filename}.${ext}`)
-}
