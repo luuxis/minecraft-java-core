8f82a7d6e620e6ceab8bb6904596c280a2e2010e
andre <61792917+luuxis@users.noreply.github.com>
2022-01-30 13:29:39 +0100

    start game elecrton

--- exemple/electron/app/index.js
+++ exemple/electron/app/index.js
@@ -2,5 +2,24 @@ const { ipcRenderer } = require('electron')
 
 
 document.querySelector('.microsoft').addEventListener('click', async () => {
+    document.querySelector('.email').disabled = true
+    document.querySelector('.password').disabled = true
+    document.querySelector('.microsoft').disabled = true
     ipcRenderer.send('microsoft', 'login')
+    ipcRenderer.on('microsoft', (event, data) => {
+        if(data === "cancel"){
+            document.querySelector('.email').disabled = false
+            document.querySelector('.password').disabled = false
+            document.querySelector('.microsoft').disabled = false
+        } else if(data === "success"){
+            document.querySelector('.email').disabled = true
+            document.querySelector('.password').disabled = true
+            document.querySelector('.microsoft').disabled = true
+            document.querySelector('.play').disabled = false
+        }
+    })
+})
+
+document.querySelector('.play').addEventListener('click', async () => {
+    ipcRenderer.send('play', 'login')
 })
--- exemple/electron/index.js
+++ exemple/electron/index.js
@@ -1,7 +1,8 @@
 const {app, BrowserWindow, ipcMain } = require('electron')
-const { microsoft, mojang, launch, status } = require('minecraft-java-core')
+const { microsoft, mojang, launch } = require('minecraft-java-core')
 const Launch = new launch()
 const Microsoft = new microsoft()
+let authenticator
 
 
 function createWindow () {
@@ -9,8 +10,8 @@ function createWindow () {
     width: 400,
     height: 500,
     webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false
+      nodeIntegration: true,
+      contextIsolation: false
     }
   })
   mainWindow.webContents.openDevTools()
@@ -32,8 +33,40 @@ app.on('window-all-closed', function () {
 
 ipcMain.on("microsoft", async (event, data) => {
     if(data === "login"){
-        console.log(await Microsoft.getAuth())
+      let login = await Microsoft.getAuth()
+      if(login){
+        event.sender.send("microsoft", "success")
+        authenticator = login
+      } else {
+        event.sender.send("microsoft", "cancel")
+      }
     }
+})
+
+ipcMain.on("play", () => {
+  let opts = {
+    authorization: authenticator,
+    path: "./AppData/.minecraft",
+    version: "20w14infinite",
+    detached: true,
+    java: true,
+    custom: false,
+    verify: false,
+    memory: {
+      min: `3G`,
+      max: `6G` 
+    }
+  }
+  
+  Launch.launch(opts)
+  
+  Launch.on('progress', (DL, totDL) => {
+    console.log(`Telechargement ${((DL / totDL) * 100).toFixed(0)}%`)
+  });
+  
+  Launch.on('data', (e) => {
+    console.log(e)
+  })
 })
 
 
