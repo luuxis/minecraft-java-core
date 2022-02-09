8464b9dbbe4d9eb6ede85494e5cf298bb9039863
Luuxis-tuto <61792917+luuxis@users.noreply.github.com>
2022-02-09 23:49:20 +0100

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,6 +1,5 @@
 const { launch, microsoft, mojang } = require('../index');
 const login = require('./login.json');
-const launcher = new launch();
 const fs = require('fs');
 
 async function main() {
@@ -9,59 +8,59 @@ async function main() {
     let mc = await mojang.getAuth('luuxis');
     console.log(JSON.stringify(mc, true, 4));
 
-    // let opts = {
-    //     url: "http://launcher.selvania.fr/files",
-    //     authorization: mc,
-    //     path: "./.Minecraft",
-    //     version: "1.18.1",
-    //     detached: false,
+    let opts = {
+        url: "http://launcher.selvania.fr/files",
+        authorization: mc,
+        path: "./.Minecraft",
+        version: "1.18.1",
+        detached: false,
 
-    //     java: true,
-    //     args: [],
-    //     custom: true,
+        java: true,
+        args: [],
+        custom: true,
 
-    //     server: {
-    //         ip: "mc.hypixel.net",
-    //         port: 25565,
-    //         autoconnect: false,
-    //     },
+        server: {
+            ip: "mc.hypixel.net",
+            port: 25565,
+            autoconnect: false,
+        },
 
-    //     verify: false,
-    //     ignored: ["options.txt", ".fabric", "config", "logs", "ressourcepacks", "shaderpacks", "crash-reports"],
+        verify: false,
+        ignored: ["options.txt", ".fabric", "config", "logs", "ressourcepacks", "shaderpacks", "crash-reports"],
 
-    //     memory: {
-    //         min: `3G`,
-    //         max: `6G` 
-    //     }
-    // }
+        memory: {
+            min: `3G`,
+            max: `6G` 
+        }
+    }
 
-    // launcher.launch(opts)
+    launch.launch(opts)
 
-    // launcher.on('progress', (DL, totDL) => {
-    //     console.log(`${(DL / 1067008).toFixed(2)} Mb to ${(totDL / 1067008).toFixed(2)} Mb`);
-    // });
+    launch.on('progress', (DL, totDL) => {
+        console.log(`${(DL / 1067008).toFixed(2)} Mb to ${(totDL / 1067008).toFixed(2)} Mb`);
+    });
 
 
-    // // launcher.on('speed', (speed) => {
-    // //     console.log(`${(speed / 1067008).toFixed(2)} Mb/s`)
-    // // })
-
-    // launcher.on('estimated', (time) => {
-    //     let hours = Math.floor(time / 3600);
-    //     let minutes = Math.floor((time - hours * 3600) / 60);
-    //     let seconds = Math.floor(time - hours * 3600 - minutes * 60);
-    //     console.log(`${hours}h ${minutes}m ${seconds}s`);
+    // launch.on('speed', (speed) => {
+    //     console.log(`${(speed / 1067008).toFixed(2)} Mb/s`)
     // })
 
+    launch.on('estimated', (time) => {
+        let hours = Math.floor(time / 3600);
+        let minutes = Math.floor((time - hours * 3600) / 60);
+        let seconds = Math.floor(time - hours * 3600 - minutes * 60);
+        console.log(`${hours}h ${minutes}m ${seconds}s`);
+    })
 
-    // launcher.on('data', (e) => {
-    //     console.log(e)
+
+    launch.on('data', (e) => {
+        console.log(e)
+    })
+
+    // launch.on('close', () => {
+    //     console.clear();
+    //     console.log("game closed");
     // })
-
-    // // launcher.on('close', () => {
-    // //     console.clear();
-    // //     console.log("game closed");
-    // // })
 }
 main();
 
