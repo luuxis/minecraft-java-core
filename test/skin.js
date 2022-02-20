3714a5df2abcf737d89d2bb69bb6a478de48087b
andre <61792917+luuxis@users.noreply.github.com>
2022-02-20 19:53:02 +0100

    add change skin

--- test/skin.js
+++ test/skin.js
@@ -1,1 +1,6 @@
 const { skin } = require('../index');
+
+((async () => {
+    let profile = await skin.skin('access_token', 'https://textures.minecraft.net/texture/ac3de5b50fb6174da51032677ead046295486bf682b3ea73e0617a210c4b4f46');
+    console.log(profile);
+})());
--- utils/skin/skin.js
+++ utils/skin/skin.js
@@ -1,4 +1,24 @@
+const fetch = require('node-fetch');
+
 class skin {
+    async skin(access_token, skin_url){
+        let profile = await fetch("https://api.minecraftservices.com/minecraft/profile/skins", {
+            method: "POST",
+            headers: {
+                'Authorization': `Bearer ${access_token}`
+            },
+            body: JSON.stringify({
+                url: skin_url,
+                variant: 'Classic'
+            }),
+        }).then(res => res.json());
+        if(profile.error) throw (`error: ${profile.error_description}`);
+        return profile;
+    }
     
 }
+
+
+
+
 module.exports = new skin;
