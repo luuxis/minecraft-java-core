46f63f8f3f1df334baa2bb9588abbb756860d4dc
andre <61792917+luuxis@users.noreply.github.com>
2022-02-20 20:17:23 +0100

    fix errer

--- test/skin.js
+++ test/skin.js
@@ -1,6 +1,32 @@
-const { skin } = require('../index');
+const { skin, microsoft } = require('../index');
+const fs = require('fs');
 
-((async () => {
-    let profile = await skin.skin('access_token', 'https://textures.minecraft.net/texture/ac3de5b50fb6174da51032677ead046295486bf682b3ea73e0617a210c4b4f46');
+let save = true;
+let client_id = '5a75d2a6-a3c0-4506-9f12-0a557534938a'
+let skin_url = 'https://cdn.discordapp.com/attachments/877154497233813524/945035875459432548/skin.png';
+let mc
+
+async function main() {
+    if (save) {
+        if (!fs.existsSync('./account.json')) {
+            mc = await new microsoft(client_id).getAuth();
+            fs.writeFileSync('./account.json', JSON.stringify(mc, true, 4));
+        } else {
+            mc = JSON.parse(fs.readFileSync('./account.json'));
+        }
+        if(!mc.refresh_token){
+            mc = await new microsoft(client_id).getAuth();
+            fs.writeFileSync('./account.json', JSON.stringify(mc, true, 4));
+        } else {
+            mc = await new microsoft(client_id).refresh(mc);
+            fs.writeFileSync('./account.json', JSON.stringify(mc, true, 4));
+        }
+    } else {
+        mc = await new microsoft(client_id).getAuth();
+    }
+
+    let profile = await skin.skin(mc.access_token, skin_url);
     console.log(profile);
-})());
+
+}
+main();
--- utils/skin/skin.js
+++ utils/skin/skin.js
@@ -5,20 +5,18 @@ class skin {
         let profile = await fetch("https://api.minecraftservices.com/minecraft/profile/skins", {
             method: "POST",
             headers: {
-                'Authorization': `Bearer ${access_token}`
+                'Authorization': `Bearer ${access_token}`,
+                'Content-Type': 'multipart/form-data',
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify({
                 url: skin_url,
                 variant: 'Classic'
             }),
         }).then(res => res.json());
-        if(profile.error) throw (`error: ${profile.error_description}`);
+        if(profile.error) throw (`error: ${profile.errorType}`);
         return profile;
     }
     
 }
-
-
-
-
 module.exports = new skin;
