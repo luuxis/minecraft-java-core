3809241e3c0c58d6d8e41548cd8786788118c159
andre <61792917+luuxis@users.noreply.github.com>
2022-02-21 23:42:46 +0100

    add SkinChangeURL, SkinDelete

--- utils/skin/skin.js
+++ utils/skin/skin.js
@@ -2,7 +2,23 @@ const fetch = require('node-fetch');
 
 class skin {
 
-    async skin(data){
+    async SkinChange(data){
+        let skin = await fetch("https://api.minecraftservices.com/minecraft/profile/skins", {
+            method: "POST",
+            headers: {
+                'Authorization': `Bearer ${data.access_token}`,
+                'Content-Type': 'multipart/form-data'
+            },
+            body: JSON.stringify({
+                variant: data.slim ? 'slim' : 'Classic',
+                file: data.data_skin
+            }),
+        }).then(res => res.json());
+        if(skin.error) throw (`error: ${skin.errorType}`);
+        return skin;
+    }
+
+    async SkinChangeURL(data){
         let skin = await fetch("https://api.minecraftservices.com/minecraft/profile/skins", {
             method: "POST",
             headers: {
@@ -11,8 +27,19 @@ class skin {
             },
             body: JSON.stringify({
                 variant: data.slim ? 'slim' : 'Classic',
-                url: data.skin_url
+                url: data.data_skin
             }),
+        }).then(res => res.json());
+        if(skin.error) throw (`error: ${skin.errorType}`);
+        return skin;
+    }
+
+    async SkinDelete(data){
+        let skin = await fetch("https://api.minecraftservices.com/minecraft/profile/skins/active", {
+            method: "DELETE",
+            headers: {
+                'Authorization': `Bearer ${data.access_token}`,
+            }
         }).then(res => res.json());
         if(skin.error) throw (`error: ${skin.errorType}`);
         return skin;
