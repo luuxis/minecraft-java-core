066ff9899cd45f0feb8948ff44d028d30e508381
andre <61792917+luuxis@users.noreply.github.com>
2022-02-26 08:19:27 +0100

    Update skin.js

--- test/skin.js
+++ test/skin.js
@@ -3,9 +3,7 @@ const { skin, microsoft } = require('../index');
 const fs = require('fs');
 
 let save = true;
-let client_id = '5a75d2a6-a3c0-4506-9f12-0a557534938a'
-let base64 = fs.readFileSync('./.Minecraft/skin.png', 'base64');
-let buffer = Buffer.from(base64, 'base64')
+let client_id = '5a75d2a6-a3c0-4506-9f12-0a557534938a';
 let mc
 
 async function main() {
@@ -30,13 +28,13 @@ async function main() {
 
     let conf = {
         access_token: mc.access_token, // Your access token
-        data_skin: buffer, // Your skin url
+        data_skin: skin.buffer('./.Minecraft/skin.png'), // Your skin url
         slim: false, // true for slim skin, false for classic skin
         cape_id: '', // Your cape id
     }
 
     console.log(conf)
-    // console.log(await skin.SkinChangeUpload(conf));
+    console.log(await skin.SkinChangeUpload(conf));
     // console.log(await skin.SkinChangeURL(conf));
     // console.log(await skin.SkinDelete(conf));
     // console.log(await skin.CapeChange(conf));
