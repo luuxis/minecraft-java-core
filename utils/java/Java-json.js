05e3425942d73b6a4cfe210c3ecc2a7e5f4fb6f4
andre <61792917+luuxis@users.noreply.github.com>
2022-05-05 10:19:08 +0200

    change name java folder

--- utils/java/Java-json.js
+++ utils/java/Java-json.js
@@ -3,6 +3,7 @@ const fetch = require("node-fetch");
 
 module.exports.GetJsonJava = async function(minecraftVersion) {
     let files = [];
+    let version
     let javaVersionsJson = await fetch("https://launchermeta.mojang.com/v1/products/java-runtime/2ec0cc96c44e5a76b9c8b7c39df7210883d12871/all.json").then(res => res.json())
     let jsonversion = (await fetch("https://launchermeta.mojang.com/mc/game/version_manifest_v2.json").then(res => res.json())).versions.find(ver => ver.id == minecraftVersion);
 
@@ -19,12 +20,15 @@ module.exports.GetJsonJava = async function(minecraftVersion) {
 
     if (os.platform() == "win32") {
         let arch = { x64: "windows-x64", ia32: "windows-x86" }
+        version = `jre-${javaVersionsJson[`${arch[os.arch()]}`][jsonversion][0].version.name}`
         javaVersionsJson = Object.entries((await fetch(javaVersionsJson[`${arch[os.arch()]}`][jsonversion][0].manifest.url).then(res => res.json())).files)
     } else if (os.platform() == "darwin") {
         let arch = { x64: "mac-os", aarch64: "mac-os-arm64" }
-        javaVersionsJson = Object.entries((await fetch(javaVersionsJson[`${arch[os.arch()]}`][jsonversion][0].manifest.url).then(res => res.json())).files)
+        version = `jre-${javaVersionsJson[`${arch[os.arch()]}`][jsonversion][0].version.name}`
+        javaVersionsJson = Object.entries((await fetch(javaVersionsJson[`${arch[os.arch()]}`][jsonversion][0].manifest.url).then(res => res.json())).files)   
     } else if (os.platform() == "linux") {
         let arch = { x64: "linux", ia32: "linux-i386" }
+        version = `jre-${javaVersionsJson[`${arch[os.arch()]}`][jsonversion][0].version.name}`
         javaVersionsJson = Object.entries((await fetch(javaVersionsJson[`${arch[os.arch()]}`][jsonversion][0].manifest.url).then(res => res.json())).files)
     } else {
         return console.log("OS not supported");
@@ -37,7 +41,7 @@ module.exports.GetJsonJava = async function(minecraftVersion) {
         if (info.type == "directory") continue;
         if (!info.downloads) continue;
         let file = {};
-        file.path = `runtime/${jsonversion}/${path.replace(toDelete, "")}`;
+        file.path = `runtime/${version}/${path.replace(toDelete, "")}`;
         file.folder = file.path.split("/").slice(0, -1).join("/");
         file.executable = info.executable;
         file.sha1 = info.downloads.raw.sha1;
@@ -46,5 +50,5 @@ module.exports.GetJsonJava = async function(minecraftVersion) {
         file.type = "JAVA";
         files.push(file);
     }
-    return { files: files, version: jsonversion };
+    return { files: files, version: version };
 }
