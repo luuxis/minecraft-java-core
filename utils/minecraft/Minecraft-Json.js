52c2cb4bfa8bd49b6c7b10a9888afc5f443abdb2
andre <61792917+luuxis@users.noreply.github.com>
2022-05-17 03:21:49 +0200

    add get version_manifest_v2.json

--- utils/minecraft/Minecraft-Json.js
+++ utils/minecraft/Minecraft-Json.js
@@ -1,5 +1,30 @@
-class json {
+const fetch = require('node-fetch');
 
+class json {
+    constructor(version) {
+        this.version = version;
+    }
+
+    async GetInfoVersion() {
+        let data = await fetch('https://launchermeta.mojang.com/mc/game/version_manifest_v2.json');
+        data = await data.json();
+
+        if (this.version == 'latest_release') {
+            this.version = data.latest.release;
+        } else if (this.version == 'latest_snapshot') {
+            this.version = data.latest.snapshot;
+        }
+        
+        data = data.versions.find(v => v.id === this.version);
+
+        if (!data) {
+            return {
+                error: true,
+                message: `version ${this.version} not found`
+            };
+        }
+        return data;
+    }
 }
 
 module.exports = json;
