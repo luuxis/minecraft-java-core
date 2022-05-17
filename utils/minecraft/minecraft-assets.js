14fdfaada0f0014c1f35da7bcc70d936a5bee946
andre <61792917+luuxis@users.noreply.github.com>
2022-05-17 03:24:42 +0200

    add assetet download

--- utils/minecraft/minecraft-assets.js
+++ utils/minecraft/minecraft-assets.js
@@ -1,5 +1,21 @@
 class assets {
+    constructor(assetIndex) {
+        this.assetIndex = assetIndex;
+    }
 
+    async Getassets() {
+        let assets = [];
+        for (let asset of Object.values(this.assetIndex.objects)) {
+            assets.push({
+                sha1: asset.hash,
+                size: asset.size,
+                type: "FILE",
+                path: `assets/objects/${asset.hash.substring(0, 2)}/${asset.hash}`,
+                url: `https://resources.download.minecraft.net/${asset.hash.substring(0, 2)}/${asset.hash}`
+            });
+        }
+        return assets;
+    }
 }
 
 module.exports = assets;
--- utils/minecraft/minecraft-libraries.js
+++ utils/minecraft/minecraft-libraries.js
@@ -1,5 +1,43 @@
-class libraries {
 
+const os = require('os');
+
+let MojangLib = { win32: "windows", darwin: "osx", linux: "linux" };
+let Arch = { x32: "32", x64: "64", arm: "32", arm64: "64" };
+
+class libraries {
+    constructor(version) {
+        this.version = version;
+    }
+    async Getlibraries() {
+        let libraries = [];
+        for (let lib of this.version.libraries) {
+            let artifact;
+            let type = "LIBRARY"
+            if (lib.natives) {
+                let classifiers = lib.downloads.classifiers;
+                let native = lib.natives[MojangLib[process.platform]];
+                if (!native) native = lib.natives[process.platform];
+                type = "NATIVE";
+                if (native) artifact = classifiers[native.replace("${arch}", Arch[os.arch()])];
+                else continue;
+            } else {
+                if (lib.rules && lib.rules[0].os) {
+                    if (lib.rules[0].os.name !== "")
+                        continue;
+                }
+                artifact = lib.downloads.artifact;
+            }
+            if (!artifact) continue;
+            libraries.push({
+                sha1: artifact.sha1,
+                size: artifact.size,
+                path: `libraries/${artifact.path}`,
+                type: type,
+                url: artifact.url
+            });
+        }
+        return libraries;
+    }
 }
 
-module.exports = args;
+module.exports = libraries;
