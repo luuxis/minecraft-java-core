e49ded804854037e9eb61cf13bc2836f1c4fb64f
andre <61792917+luuxis@users.noreply.github.com>
2022-05-17 21:39:40 +0200

    init launch file

--- utils/launch.js
+++ utils/launch.js
@@ -1,134 +1,49 @@
 'use strict';
-const download = require('./download.js');
-const Handler = require('./minecraft/Minecraft-Json.js');
-const start = require('./minecraft/Minecraft-start.js');
-
+// import librairies nodejs
 const path = require('path');
-const fs = require('fs');
+const fetch = require('node-fetch');
+
+//import modules minecraft-java-core
+const gameJsonMinecraft = require('./Minecraft-utils/Minecraft-Json');
+const gameAssetsMinecraft = require('./Minecraft-utils/Minecraft-Assets');
+const gameLibrariesMinecraft = require('./Minecraft-utils/Minecraft-Libraries');
 
 class Launch {
-    async launch(options) {
-        this.options = options;
-        if (this.options.javapath) {
-            this.options.java = false;
-            this.options.ignored.push(this.options.javapath);
+    constructor(config = {}) {
+        this.config = {
+            url: config.url ? config.url : null,
+            authenticator: config.authenticator ? config.authenticator : null,
+            path: path.resolve(config.path ? config.path : './Minecraft').replace(/\\/g, '/'),
+            version: config.version ? config.version : 'latest_release',
+
+            custom: config.custom ? config.custom : false,
+            verify: config.verify ? config.verify : false,
+            ignored: config.ignored ? config.ignored : null,
+            java: config.java ? config.java : null,
+
+            memory: {
+                min: config.memory?.min ? config.memory.min : '1G',
+                max: config.memory?.max ? config.memory.max : '2G'
+            }
         }
-        this.options.authorization = await Promise.resolve(this.options.authorization);
-        this.jsonversion = new Handler(options);
-        this.java_version = await this.jsonversion.java_version();
-        this.checkFiles();
+
+        this.Launch()
     }
 
-    async checkFiles() {
-        this.files = await this.jsonversion.getJSONVersion(this.options.version);
-        if (this.options.verify) this.jsonversion.removeNonIgnoredFiles(this.files);
-        let todownload = await this.jsonversion.checkBundle(this.options.version)
-
-        if (todownload.length > 0) {
-            let downloader = new download();
-            let totsize = this.jsonversion.getTotalSize(todownload);
-
-            downloader.on("progress", (DL, totDL) => {
-                this.emit("progress", DL, totDL);
-            });
-
-            downloader.on("speed", (speed) => {
-                this.emit("speed", speed);
-            });
-
-            downloader.on("estimated", (time) => {
-                this.emit("estimated", time);
-            });
-
-            await new Promise((ret) => {
-                downloader.on("finish", ret);
-                downloader.multiple(todownload, totsize, 10);
-            });
-        }
-
-        if (this.options.verify) this.jsonversion.removeNonIgnoredFiles(this.files);
-        this.jsonversion.natives(this.files);
-        this.startgame();
+    async Launch() {
+        let gameJson = await this.GetJsonVersion();
+        let gameAssets = await new gameAssetsMinecraft(gameJson.json.assetIndex.url).Getassets();
+        let gameLibraries = await new gameLibrariesMinecraft(gameJson.json).Getlibraries();
+        return { gameJson: gameJson, gameAssets: gameAssets, gameLibraries: gameLibraries };
     }
 
-    async startgame() {
-        this.path = (`${path.resolve(this.options.path)}`).replace(/\\/g, "/")
-        this.natives = `${this.path}/versions/${this.options.version}/natives`
-
-        this.vanilla = require(this.files.filter(mod => mod.type == "VERSION").map(mod => `${this.path}/${mod.path}`)[0])
-        if (this.options.custom === true) this.custom = require(this.files.filter(mod => mod.type == "VERIONSCUSTOM").map(mod => `${this.path}/${mod.path}`)[0])
-
-        this.json = this.vanilla
-        this.json.id = this.files.filter(mod => mod.type == "JARVERSION").map(mod => `${this.path}/${mod.path}`)[0]
-        this.json.mainClass = this.vanilla.mainClass
-        if (this.options.custom === true) {
-            this.json.custom = this.custom
-            this.json.mainClass = this.custom.mainClass
+    async GetJsonVersion() {
+        let InfoVersion = await new gameJsonMinecraft(this.config.version).GetInfoVersion();
+        if (InfoVersion.error) {
+            return InfoVersion;
         }
-
-        let source = {
-            natives: this.natives,
-            json: this.json,
-            authenticator: this.options.authenticator,
-            root: this.path,
-        }
-
-        this.start = new start(this.options, source);
-
-        let args = await this.start.agrs();
-
-        let jvm = args.jvm
-        let classPaths = args.classPaths
-        let launchOptions = args.launchOptions
-
-        let launchargs = [].concat(jvm, classPaths, launchOptions)
-        let java;
-
-        if (this.options.java) {
-            java = `${this.path}/runtime/${this.java_version}/bin/java`;
-        } else if (this.options.javapath) {
-            java = (`${path.resolve(this.options.javapath)}`).replace(/\\/g, "/");
-        } else {
-            java = "java";
-        }
-
-        if (this.start.isold()) {
-            const legacyDirectory = `${this.path}/resources`;
-            const index = require(`${this.path}/assets/indexes/${this.json.assets}`);
-            if (!fs.existsSync(legacyDirectory)) fs.mkdirSync(legacyDirectory, { recursive: true });
-
-            await Promise.all(Object.keys(index.objects).map(async asset => {
-                const hash = index.objects[asset].hash
-                const subhash = hash.substring(0, 2)
-                const subAsset = `${this.path}/assets/objects/${subhash}`
-
-                const legacyAsset = asset.split('/')
-                legacyAsset.pop()
-
-                if (!fs.existsSync(path.join(legacyDirectory, legacyAsset.join('/')))) {
-                    fs.mkdirSync(path.join(legacyDirectory, legacyAsset.join('/')), { recursive: true })
-                }
-
-                if (!fs.existsSync(path.join(legacyDirectory, asset))) {
-                    fs.copyFileSync(path.join(subAsset, hash), path.join(legacyDirectory, asset))
-                }
-            }))
-        }
-
-        this.emit('data', `Launching with arguments ${launchargs.join(' ')}`)
-        let game = this.start.start(launchargs, java)
-        game.stdout.on('data', (data) => this.emit('data', data.toString('utf-8')))
-        game.stderr.on('data', (data) => this.emit('data', data.toString('utf-8')))
-        game.on('close', (code) => this.emit('close', code))
-    }
-
-    on(event, func) {
-        this[event] = func;
-    }
-
-    emit(event, ...args) {
-        if (this[event]) this[event](...args);
+        let json = await fetch(InfoVersion.url).then(res => res.json());
+        return { InfoVersion: InfoVersion, json: json };
     }
 }
-
-module.exports = new Launch;
+module.exports = Launch;
