a6dadbae7d6ba58ab7a5639a9069e9898d359b8d
andre <61792917+luuxis@users.noreply.github.com>
2022-05-17 21:39:51 +0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,18 +1,24 @@
-const JsonVersion = require('../utils/minecraft/Minecraft-Json.js');
-const assetsVertion = require('../utils/minecraft/Minecraft-assets.js');
-const librariesVertion = require('../utils/minecraft/minecraft-libraries');
-const fetch = require('node-fetch')
-const fs = require('fs')
+const { Launch } = require('../index');
+const fs = require('fs');
 
 async function test_json() {
-    let data = await new JsonVersion('latest_snapshot').GetInfoVersion();
-    let version = await fetch(data.url).then(data => data.json())
-    let assets = await fetch(version.assetIndex.url).then(data => data.json())
-    let assetDownload = await new assetsVertion(assets).Getassets();
-    let librariesDownload = await new librariesVertion(version).Getlibraries();
-    // console.log(librariesDownload);
-    
-    fs.writeFileSync('latest_snapshot_lib.json', JSON.stringify(librariesDownload, null, 4))
+    let a = await new Launch({
+        url: "http://launcher.selvania.fr/files",
+        path: "./.Minecraft",
+        version: "latest_snapshot",
+        detached: false,
+        java: true,
+        args: [],
+        custom: false,
+        verify: false,
+        ignored: ["crash-reports", "logs", "resourcepacks", "resources", "saves", "shaderpacks", "options.txt", "optionsof.txt"],
 
+        memory: {
+            min: `1G`,
+            max: `2G`
+        }
+    }).Launch();
+
+    fs.writeFileSync('./test.json', JSON.stringify(a, null, 4));
 }
 test_json();
