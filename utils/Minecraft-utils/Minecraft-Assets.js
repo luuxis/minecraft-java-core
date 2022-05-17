78e7d1abca83e96c7cd031915ef74ad6ea89f241
andre <61792917+luuxis@users.noreply.github.com>
2022-05-17 21:45:51 +0200

    assetIndex >  assetIndexUrl

--- utils/Minecraft-utils/Minecraft-Assets.js
+++ utils/Minecraft-utils/Minecraft-Assets.js
@@ -1,14 +1,14 @@
 'use strict';
 const fetch = require('node-fetch');
 
-class assets {
-    constructor(assetIndex) {
-        this.assetIndex = assetIndex;
+class Assets {
+    constructor(assetIndexUrl) {
+        this.assetIndexUrl = assetIndexUrl;
     }
 
     async Getassets() {
         let assets = [];
-        let data = await fetch(this.assetIndex).then(res => res.json());
+        let data = await fetch(this.assetIndexUrl).then(res => res.json());
         for (let asset of Object.values(data.objects)) {
             assets.push({
                 sha1: asset.hash,
@@ -22,4 +22,4 @@ class assets {
     }
 }
 
-module.exports = assets;
+module.exports = Assets;
