61d18f6b9811801e967785f94b4c90c0442b72b4
andre <61792917+luuxis@users.noreply.github.com>
2022-05-18 03:40:28 +0200

    add detached option

--- utils/launch.js
+++ utils/launch.js
@@ -7,19 +7,24 @@ const fetch = require('node-fetch');
 const gameJsonMinecraft = require('./Minecraft-utils/Minecraft-Json');
 const gameAssetsMinecraft = require('./Minecraft-utils/Minecraft-Assets');
 const gameLibrariesMinecraft = require('./Minecraft-utils/Minecraft-Libraries');
+const gameVerifyMinecraft = require('./Minecraft-utils/Minecraft-Verify');
 
 class Launch {
-    constructor(config = {}) {
+    async Launch(config = {}) {
         this.config = {
             url: config.url ? config.url : null,
             authenticator: config.authenticator ? config.authenticator : null,
             path: path.resolve(config.path ? config.path : './Minecraft').replace(/\\/g, '/'),
             version: config.version ? config.version : 'latest_release',
+            detached: config.detached ? config.detached : false,
 
             custom: config.custom ? config.custom : false,
             verify: config.verify ? config.verify : false,
-            ignored: config.ignored ? config.ignored : null,
-            java: config.java ? config.java : null,
+            ignored: config.ignored ? config.ignored : [],
+            args: config.args ? config.args : [],
+
+            javaPath: config.javaPath ? config.javaPath : null,
+            java: config.java ? config.java : true,
 
             memory: {
                 min: config.memory?.min ? config.memory.min : '1G',
@@ -27,14 +32,11 @@ class Launch {
             }
         }
 
-        this.Launch()
-    }
-
-    async Launch() {
         let gameJson = await this.GetJsonVersion();
         let gameAssets = await new gameAssetsMinecraft(gameJson.json.assetIndex.url).Getassets();
         let gameLibraries = await new gameLibrariesMinecraft(gameJson.json).Getlibraries();
-        return { gameJson: gameJson, gameAssets: gameAssets, gameLibraries: gameLibraries };
+        let gameVerify = await new gameVerifyMinecraft([...gameLibraries, ...gameAssets.assets], this.config).checkBundle();
+        return gameVerify
     }
 
     async GetJsonVersion() {
