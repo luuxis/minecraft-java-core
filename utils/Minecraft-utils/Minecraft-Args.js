6a10d9cfbcdd04303c7c88fa0f4de2b36f41d7ba
andre <61792917+luuxis@users.noreply.github.com>
2022-05-19 00:54:51 +0200

    add args start minecraft

--- utils/Minecraft-utils/Minecraft-Args.js
+++ utils/Minecraft-utils/Minecraft-Args.js
@@ -1,6 +1,31 @@
 'use strict';
 class Args {
+    constructor(json, config) {
+        this.json = json;
+        this.config = config;
+        this.version = this.json.id;
+    }
+    GetArgs() {
+        let args = this.json.minecraftArguments ? this.json.minecraftArguments.split(' ') : this.json.arguments.game;
+        let table = {
+            '${auth_player_name}': '',
+            '${version_name}': '',
+            '${game_directory}': '',
+            '${assets_root}': '',
+            '${assets_index_name}': '',
+            '${auth_uuid}': this.config.authenticator.uuid,
+            '${auth_access_token}': this.config.authenticator.access_token,
+            '${clientid}': this.config.authenticator.client_token,
+            '${auth_xuid}': '',
+            '${user_type}': '',
+            '${version_type}': '',
+        }
 
+
+        console.log(this.config.authenticator)
+
+        return args
+    }
 }
 
 module.exports = Args;
