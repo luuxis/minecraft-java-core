8646d7f58108d9a5b1529b572725886e3fff7933
andre <61792917+luuxis@users.noreply.github.com>
2022-08-17 16:17:53 +0200

    add download lib missing

--- utils/Minecraft-utils/Minecraft-Modde.js
+++ utils/Minecraft-utils/Minecraft-Modde.js
@@ -12,38 +12,91 @@ module.exports = class modde {
         this.config = config;
     }
 
-    async filesGameModde() {
+    async librairiesModdeJson(ModdeJson) {
+        let libModde = ModdeJson.libraries.map(librarie => librarie.name);
+        let allLibrairiesModde = []
+        for (let lib of libModde) {
+            let libSplit = lib.split(':')
+            let libName = `${libSplit[1]}-${libSplit[2]}.jar`
+            let pathLib = `${libSplit[0].replace(/\./g, '/')}/${libSplit[1]}/${libSplit[2]}/${libName}`
+            allLibrairiesModde.push(pathLib)
+        }
+        return allLibrairiesModde
+    }
+
+    async fileInfo(url) {
+        let response = await nodeFetch(url);
+        if(response.status == 200) {
+            let size = response.headers.get('content-length');
+            return {
+                size: parseInt(size),
+                sha1: response.headers.get('x-minecraft-sha1')
+            }
+        } else {
+            return {
+                size: '',
+                sha1: ''
+            }
+        }
+    }
+
+    async filesList(ModdeJson, filesServer) {
+        let forgeURL = 'https://files.minecraftforge.net/maven';
+        let librairiesModde = await this.librairiesModdeJson(ModdeJson);
+        let LibrairiesServer = filesServer.filter(file => file.type == 'MODDELIBRARY');
+        LibrairiesServer = LibrairiesServer.map(file => file.path);
+        let missing = librairiesModde.filter(Librairies => !LibrairiesServer.includes(`libraries/${Librairies}`));
+
+        let filesList = [];
+        for (let lib of missing) {
+            let fileProperties = await this.fileInfo(`${forgeURL}/${lib}`);
+            let file = {}
+            file.path = `libraries/${lib}`,
+            file.size = fileProperties.size,
+            file.sha1 = fileProperties.sha1,
+            file.url = `${forgeURL}/${lib}`,
+            file.type = 'MODDELIBRARY'
+            filesList.push(file);
+        }
+
+        console.log(filesList)
+        return []//filesList;
+    }
+
+    async filesGame() {
         if (this.config.modde) {
             if (this.config.url == null) return [];
-            let json = await nodeFetch(this.config.url).then(res => res.json());
+
+            let filesServer = await nodeFetch(this.config.url).then(res => res.json());
+            let ModdeJson = await nodeFetch(filesServer.filter(file => file.type == 'VERIONSCUSTOM').map(file => file.url)[0]).then(res => res.json());
+            let filesList = await this.filesList(ModdeJson, filesServer);
+
             let files = [];
-            for (let modde of json) {
+            for (let modde of [...filesServer, ...filesList]) {
                 if (!modde.url) continue;
                 let file = {}
                 file.path = modde.path,
-                    file.size = modde.size,
-                    file.sha1 = modde.sha1,
-                    file.url = modde.url,
-                    file.type = modde.type
+                file.size = modde.size,
+                file.sha1 = modde.sha1,
+                file.url = modde.url,
+                file.type = modde.type
                 files.push(file);
             }
-            return files;
-        } else return [];
-    }
-
-    async jsonModde() {
-        if (this.config.modde) {
-            let files = await this.filesGameModde();
-            let urlModdejson = files.filter(file => file.type == 'VERIONSCUSTOM').map(file => file.url)[0];
-            let jsonModde = await nodeFetch(urlModdejson);
-            return jsonModde.json();
-        } else return false;
+            return {
+                files: files,
+                ModdeJson: ModdeJson
+            }
+        } else return {
+            files: [],
+            ModdeJson: false
+        };
     }
 
     async GameModde() {
+        let filesGame = await this.filesGame();
         return {
-            gameModdeFiles: await this.filesGameModde(),
-            gameModdeJson: await this.jsonModde()
+            gameModdeFiles: filesGame.files,
+            gameModdeJson: filesGame.ModdeJson
         }
     }
 }
