74902a53b714cec6b031d8a2586cd7cca5e910d9
andre <61792917+luuxis@users.noreply.github.com>
2023-01-15 22:00:57 +0100

    fix

--- src/Authenticator/Mojang.ts
+++ src/Authenticator/Mojang.ts
@@ -7,154 +7,131 @@ import crypto from 'crypto';
 import nodeFetch from 'node-fetch';
 
 let api_url = 'https://authserver.mojang.com';
-let auth: any
 
 
-function login(username: string, password?: string) {
+async function login(username: string, password?: string) {
     let UUID = crypto.randomBytes(16).toString('hex');
-
-    new Promise(async (resolve, reject) => {
-        if (!password) {
-            auth = {
-                access_token: UUID,
-                client_token: UUID,
-                uuid: UUID,
-                name: username,
-                user_properties: '{}',
-                meta: {
-                    offline: true,
-                    type: 'Mojang'
-                }
-            }
-            return resolve(auth);
-        }
-
-        let post = {
-            agent: {
-                name: "Minecraft",
-                version: 1
-            },
-            username,
-            password,
-            clientToken: UUID,
-            requestUser: true
-        }
-
-        let message = await nodeFetch(`${api_url}/authenticate`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(post)
-        }).then(res => res.json());
-
-        if (message.error) {
-            auth = message;
-            return reject(message);
-        };
-        let user = {
-            access_token: message.accessToken,
-            client_token: message.clientToken,
-            uuid: message.selectedProfile.id,
-            name: message.selectedProfile.name,
+    if (!password) {
+        return {
+            access_token: UUID,
+            client_token: UUID,
+            uuid: UUID,
+            name: username,
             user_properties: '{}',
             meta: {
-                offline: false,
+                offline: true,
                 type: 'Mojang'
             }
         }
-        auth = user;
-        return resolve(user);
-    });
-    return auth;
+    }
+
+    let post = {
+        agent: {
+            name: "Minecraft",
+            version: 1
+        },
+        username,
+        password,
+        clientToken: UUID,
+        requestUser: true
+    }
+
+    let message = await nodeFetch(`${api_url}/authenticate`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(post)
+    }).then(res => res.json());
+
+    if (message.error) {
+        return message;
+    };
+    let user = {
+        access_token: message.accessToken,
+        client_token: message.clientToken,
+        uuid: message.selectedProfile.id,
+        name: message.selectedProfile.name,
+        user_properties: '{}',
+        meta: {
+            offline: false,
+            type: 'Mojang'
+        }
+    }
+    return user;
 }
 
 async function refresh(acc: any) {
-    new Promise(async (resolve, reject) => {
-        let post = {
-            accessToken: acc.access_token,
-            clientToken: acc.client_token,
-            requestUser: true
-        }
-        let message = await nodeFetch(`${api_url}/refresh`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(post)
-        }).then(res => res.json());
+    let post = {
+        accessToken: acc.access_token,
+        clientToken: acc.client_token,
+        requestUser: true
+    }
+    let message = await nodeFetch(`${api_url}/refresh`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(post)
+    }).then(res => res.json());
 
-        if (message.error) {
-            auth = message;
-            return reject(message);
-        };
+    if (message.error) {
+        return message;
+    };
 
-        let user = {
-            access_token: message.accessToken,
-            client_token: message.clientToken,
-            uuid: message.selectedProfile.id,
-            name: message.selectedProfile.name,
-            user_properties: '{}',
-            meta: {
-                offline: false,
-                type: 'Mojang'
-            }
+    let user = {
+        access_token: message.accessToken,
+        client_token: message.clientToken,
+        uuid: message.selectedProfile.id,
+        name: message.selectedProfile.name,
+        user_properties: '{}',
+        meta: {
+            offline: false,
+            type: 'Mojang'
         }
-        auth = user;
-        return resolve(user);
-    });
-    return auth;
+    }
+    return user;
 }
 
 async function validate(acc: any) {
-    new Promise(async (resolve, reject) => {
-        let post = {
-            accessToken: acc.access_token,
-            clientToken: acc.client_token,
-        }
-        let message = await nodeFetch(`${api_url}/validate`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(post)
-        });
-
-        if (message.status == 204) {
-            auth = true
-            return resolve(true);
-        } else {
-            auth = false
-            return reject(false);
-        }
+    let post = {
+        accessToken: acc.access_token,
+        clientToken: acc.client_token,
+    }
+    let message = await nodeFetch(`${api_url}/validate`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(post)
     });
-    return auth;
+
+    if (message.status == 204) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 async function signout(acc: any) {
-    new Promise(async (resolve, reject) => {
-        let post = {
-            accessToken: acc.access_token,
-            clientToken: acc.client_token,
-        }
+    let post = {
+        accessToken: acc.access_token,
+        clientToken: acc.client_token,
+    }
 
-        let message = await nodeFetch(`${api_url}/invalidate`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(post)
-        }).then(res => res.text());
+    let message = await nodeFetch(`${api_url}/invalidate`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(post)
+    }).then(res => res.text());
 
-        if (message == "") {
-            auth = true
-            return resolve(true);
-        } else {
-            auth = false
-            return reject(false);
-        }
-    });
-    return auth;
+    if (message == "") {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function ChangeAuthApi(url: string) {
