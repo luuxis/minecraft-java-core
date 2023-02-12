fcbc115b51066ff670fa9db325212d464ff6b4e2
andre <61792917+luuxis@users.noreply.github.com>
2023-02-12 01:41:02 +0100

    get instance game

--- webfiles/index.php
+++ webfiles/index.php
@@ -1,23 +1,7 @@
 <?php
-function dirToArray($dir) {
-   $res = array();
-   $cdir = scandir($dir);
-   foreach ($cdir as $key => $value){
-      if (!in_array($value,array(".",".."))){
-         if (is_dir(str_replace("\\", "/", $dir . DIRECTORY_SEPARATOR . $value))) {
-            dirToArray(str_replace("\\", "/", $dir . DIRECTORY_SEPARATOR . $value));
-         } else {
-            $hash = hash_file('sha1', $dir . "/" . $value);
-            $size = filesize($dir . "/" . $value);
-            $path = str_replace("files/", "", $dir . "/" . $value);
-            $url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . $dir . "/" . $value;
-
-            echo "{\"path\":\"$path\",\"size\":$size,\"sha1\":\"$hash\",\"url\":\"$url\"},";
-         }
-      }
-   }
-}
-
 header("Content-Type: application/json; charset=UTF-8");
-echo "[", dirToArray("files"), "\"\"]";
+include 'php/scandir.php';
+
+$instance = $_GET['instance'] ?? 'files';
+echo dirToArray($instance);
 ?>
