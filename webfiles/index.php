03db8695f8c492cd3ddbed8fa0781ff24e8e9be3
andre <61792917+luuxis@users.noreply.github.com>
2023-02-12 02:01:27 +0100

    patch Security

--- webfiles/index.php
+++ webfiles/index.php
@@ -3,5 +3,10 @@ header("Content-Type: application/json; charset=UTF-8");
 include 'php/scandir.php';
 
 $instance = $_GET['instance'] ?? 'files';
+
+if ($instance[0] === '.') {
+    echo json_encode([]);
+    exit;
+}
 echo dirToArray($instance);
 ?>
