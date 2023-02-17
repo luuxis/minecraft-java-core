ac3b28c1b0769406ac0673e9cd94ce6747cf4a67
andre <61792917+luuxis@users.noreply.github.com>
2023-02-17 05:50:21 +0100

    finish api

--- webfiles/index.php
+++ webfiles/index.php
@@ -2,19 +2,31 @@
 header("Content-Type: application/json; charset=UTF-8");
 include 'php/scandir.php';
 
-$instance = $_GET['instance'];
+$instance_param = $_GET['instance'];
 
-if ($instance == '/' || $instance[0] == '.') {
+if ($instance_param == '/' || $instance_param[0] == '.') {
     echo json_encode([]);
     exit;
-} elseif (!file_exists('instances')) {
+} 
+
+if (!file_exists('instances')) {
     echo dirToArray("files");
     exit;
-} elseif ($instance == '') {
-    echo json_encode(scanfolder("instances"));
-    exit;
-} else {
-    echo dirToArray("instances/$instance");
+} 
+
+if ($instance_param == '') {
+    $instances_list = scanFolder("instances");
+    $instance = array();
+    foreach ($instances_list as $value) {
+        if ($_SERVER['REQUEST_URI'] == "/") $_SERVER['REQUEST_URI'] = "";
+        $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]?instance=$value";
+        $instance[$value] = array("name" => $value, "url" => $url);
+    }
+    
+    include 'php/instances.php';
+    echo str_replace("\\", "", json_encode($instance));
     exit;
 }
+
+echo dirToArray("instances/$instance_param");
 ?>
