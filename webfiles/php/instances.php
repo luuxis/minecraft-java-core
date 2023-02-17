eaf5fb7dbf68334975f5d4f6e649b873333e58d4
andre <61792917+luuxis@users.noreply.github.com>
2023-02-17 05:50:21 +0100

    finish api

---
+++ webfiles/php/instances.php
@@ -0,0 +1,17 @@
+<?php
+$instance['aynor'] = array_merge($instance['aynor'], array(
+    "loadder" => array(
+        "minecraft_version" => "1.16.5",
+        "loadder_type" => "forge",
+        "loadder_version" => "latest"
+    )
+));
+
+$instance['hypixel'] = array_merge($instance['hypixel'], array(
+    "loadder" => array(
+        "minecraft_version" => "1.8.9",
+        "loadder_type" => "forge",
+        "loadder_version" => "latest"
+    )
+));
+?>
