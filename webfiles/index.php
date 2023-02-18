eaf164389e123a92d6cea279c48d4b97d07f5eeb
luuxis <luuxis8215@gmail.com>
2023-02-18 02:04:51 +0100

    Update index.php

--- webfiles/index.php
+++ webfiles/index.php
@@ -18,7 +18,7 @@ if ($instance_param == '') {
     $instances_list = scanFolder("instances");
     $instance = array();
     foreach ($instances_list as $value) {
-        if ($_SERVER['REQUEST_URI'] == "/") $_SERVER['REQUEST_URI'] = "";
+        $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], 0, -1); 
         $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]?instance=$value";
         $instance[$value] = array("name" => $value, "url" => $url);
     }
