587d8b9bfc9374828ad37162139125cf37324573
Luuxis <61792917+luuxis@users.noreply.github.com>
2025-04-25 18:13:49 +0200

    export Downloader module from index

--- src/Index.ts
+++ src/Index.ts
@@ -10,11 +10,13 @@ import Launch from './Launch.js';
 import Microsoft from './Authenticator/Microsoft.js';
 import * as Mojang from './Authenticator/Mojang.js';
 import Status from './StatusServer/status.js';
+import Downloader from './utils/Downloader.js';
 
 export {
     AZauth as AZauth,
     Launch as Launch,
     Microsoft as Microsoft,
     Mojang as Mojang,
-    Status as Status
+    Status as Status,
+    Downloader as Downloader
 };
