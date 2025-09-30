60e0f4c5e11749cd3796090b2394480041d25991
Luuxis <61792917+luuxis@users.noreply.github.com>
2025-09-30 14:18:07 +0200

    Refactor test script error handling and event listeners
    Replaces account refresh error handling with process exit on error and refactors launcher event listeners to use separate statements for improved readability.

--- test/index.js
+++ test/index.js
@@ -15,12 +15,12 @@ let mc
             fs.writeFileSync('./account.json', JSON.stringify(mc, null, 4));
         } else {
             mc = await new Microsoft().refresh(mc);
-            if (mc.error) mc = await new Microsoft().getAuth();
             fs.writeFileSync('./account.json', JSON.stringify(mc, null, 4));
+            if (mc.error) process.exit(1);
         }
     }
 
-    await launcher.Launch({
+    const opt = {
         url: "https://luuxcraft.fr/api/user/69414f32-4018-4eca-948b-109c46cd119c/instance",
         path: './minecraft',
         authenticator: mc,
@@ -45,12 +45,11 @@ let mc
             min: '14G',
             max: '16G'
         },
-    });
+    };
 
-    launcher
-        .on('progress', (progress, size) => console.log(`[DL] ${((progress / size) * 100).toFixed(2)}%`))
-        .on('patch', pacth => process.stdout.write(pacth))
-        .on('data', line => process.stdout.write(line))
-        .on('error', err => console.error(err))
-        .on('close', () => console.log('Game exited.'));
+    launcher.Launch(opt);
+    launcher.on('progress', (progress, size) => console.log(`[DL] ${((progress / size) * 100).toFixed(2)}%`));
+    launcher.on('patch', pacth => process.stdout.write(pacth));
+    launcher.on('data', line => process.stdout.write(line));
+    launcher.on('error', err => console.error(err));
 })();
